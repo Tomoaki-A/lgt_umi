4408afd585eb325993a02222f8843c4baf0d64e3
Tomoaki Asano <exsrx97@gmail.com>
2023-07-17 14:20:56 +0900

    Refactor LgtmImageList component

--- src/components/entity/LgtmImageList/index.tsx
+++ src/components/entity/LgtmImageList/index.tsx
@@ -1,12 +1,7 @@
 import clsx from "clsx";
-import Image from "next/image";
 import React from "react";
 import { getImageList } from "src/data/lgtm-image";
-import {
-  LGTM_IMAGE_CROP_HEIGHT,
-  LGTM_IMAGE_CROP_WIDTH,
-  LGTM_PARAM,
-} from "src/domain/entity/lgtm/constants";
+import LgtmImage from "./LgtmImage";
 
 async function LgtmImageList() {
   const { data } = await getImageList();
@@ -16,16 +11,7 @@ async function LgtmImageList() {
     <div className={clsx("grid", "grid-cols-3", "gap-2")}>
       {data.map((image) => {
         const { id, imageSrc } = image;
-        return (
-          <div key={id}>
-            <Image
-              src={`${imageSrc.url}${LGTM_PARAM}`}
-              alt=""
-              width={LGTM_IMAGE_CROP_WIDTH / 2}
-              height={LGTM_IMAGE_CROP_HEIGHT / 2}
-            />
-          </div>
-        );
+        return <LgtmImage key={id} imageSrc={imageSrc} />;
       })}
     </div>
   );
