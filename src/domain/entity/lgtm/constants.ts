d57fa1d3b8be1b4330faa86cd95b1d4c042cdaeb
Tomoaki Asano <exsrx97@gmail.com>
2023-07-17 14:32:47 +0900

    Upd text style

--- src/domain/entity/lgtm/constants.ts
+++ src/domain/entity/lgtm/constants.ts
@@ -1,9 +1,8 @@
-// ?fit=crop&w=800&h=600&txt=LGTM!&txt-size=78&txt-color=99ffffff&txt-align=middle,center
-
 export const LGTM_IMAGE_CROP_WIDTH = 800;
 export const LGTM_IMAGE_CROP_HEIGHT = 600;
 const LGTM_IMAGE_TEXT = "LGTM!";
 const LGTM_IMAGE_TEXT_SIZE = 78;
-const LGTM_IMAGE_TEXT_COLOR = "99ffffff";
+const LGTM_IMAGE_TEXT_COLOR = "ffffff";
+const LGTM_IMAGE_TEXT_STYLE = "fantasy,bold";
 
-export const LGTM_PARAM = `?fit=crop&w=${LGTM_IMAGE_CROP_WIDTH}&h=${LGTM_IMAGE_CROP_HEIGHT}&txt=${LGTM_IMAGE_TEXT}&txt-size=${LGTM_IMAGE_TEXT_SIZE}&txt-color=${LGTM_IMAGE_TEXT_COLOR}&txt-align=middle,center`;
+export const LGTM_PARAM = `?fit=crop&w=${LGTM_IMAGE_CROP_WIDTH}&h=${LGTM_IMAGE_CROP_HEIGHT}&txt=${LGTM_IMAGE_TEXT}&txt-size=${LGTM_IMAGE_TEXT_SIZE}&txt-color=${LGTM_IMAGE_TEXT_COLOR}&txt-align=middle,center&txt-font=${LGTM_IMAGE_TEXT_STYLE}`;
