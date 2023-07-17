72f9106d3cc8b1f6c091a1ec2905197446a0b5e5
Tomoaki Asano <exsrx97@gmail.com>
2023-07-17 17:22:35 +0900

    Use ClipCopyButton component

--- src/components/entity/LgtmImageList/LgtmImage/index.tsx
+++ src/components/entity/LgtmImageList/LgtmImage/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import ClipCopyButton from "src/components/ui-component/ClipCopyButton";
 import { LgtmImage } from "src/data/lgtm-image/model";
+import { getMarkdownImageText } from "src/domain/entity/lgtm";
 import {
   LGTM_IMAGE_CROP_HEIGHT,
   LGTM_IMAGE_CROP_WIDTH,
@@ -11,14 +13,18 @@ type Props = {
 };
 
 function LgtmImage({ imageSrc }: Props) {
+  const src = `${imageSrc.url}${LGTM_PARAM}`;
+  const clipUrl = getMarkdownImageText(src);
   return (
     <div>
-      <Image
-        src={`${imageSrc.url}${LGTM_PARAM}`}
-        alt=""
-        width={LGTM_IMAGE_CROP_WIDTH / 2}
-        height={LGTM_IMAGE_CROP_HEIGHT / 2}
-      />
+      <ClipCopyButton text={clipUrl}>
+        <Image
+          src={src}
+          alt=""
+          width={LGTM_IMAGE_CROP_WIDTH / 2}
+          height={LGTM_IMAGE_CROP_HEIGHT / 2}
+        />
+      </ClipCopyButton>
     </div>
   );
 }
