6a0ee760eb7656cb94ddf93e4a685cf3903e8a2c
Tomoaki Asano <exsrx97@gmail.com>
2023-07-23 23:19:19 +0900

    Upd config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
+/**
+ * @see https://github.com/egoist/tailwindcss-icons
+ */
+const {
+  iconsPlugin,
+  getIconCollections,
+} = require("@egoist/tailwindcss-icons");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
@@ -10,5 +18,12 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    /**
+     * @see https://github.com/egoist/tailwindcss-icons
+     */
+    iconsPlugin({
+      collections: getIconCollections(["ic"]),
+    }),
+  ],
 };
