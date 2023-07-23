845e3b454435061f923904554a55a11ee44a90fa
Tomoaki Asano <exsrx97@gmail.com>
2023-07-23 23:19:40 +0900

    Delete unused import

--- src/components/ui-component/Divider/index.tsx
+++ src/components/ui-component/Divider/index.tsx
@@ -1,5 +1,3 @@
-import clsx from "clsx";
-
 function Divider() {
   return (
     <div
---
+++ src/components/ui-component/Icons/Hart/index.tsx
@@ -0,0 +1,5 @@
+const HartIcon = () => (
+  <span className="text-2xl bg-red-700 i-ic-round-favorite"></span>
+);
+
+export default HartIcon;
