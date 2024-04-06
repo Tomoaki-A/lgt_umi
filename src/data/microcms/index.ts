cc6f58031210ff803fe77c5b92d004618f799399
Tomoaki Asano <exsrx97@gmail.com>
2024-04-06 23:11:13 +0900

    :bug: Add fallback values for environment variables

--- src/data/microcms/index.ts
+++ src/data/microcms/index.ts
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const microCmsClient = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN,
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN || "",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || "",
 });
